b2d0688c9361ac81d86bbec3e4a08f8ad967afd8
BezaleelPaul <145212633+BezaleelPaul@users.noreply.github.com>
2025-12-28 17:33:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from app.routes import router
+from app.config import Settings
 
-app = FastAPI(title="Smart Task Recommender")
+app = FastAPI(
+    title=Settings.APP_NAME,
+    version=Settings.VERSION
+)
+
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
 app.include_router(router)
-
